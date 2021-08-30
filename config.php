94eae6f851fff4bd0802cf028ff95a6c6b4de6b4
Scott Cordle <scordle@sullivan.edu>
2021-08-30 13:59:06 -0400

    correcting errors

--- config.php
+++ config.php
@@ -13,3 +13,4 @@ $dsn        = "mysql:host=$host;dbname=$dbname"; // will use later
 $options    = array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
               );
+              
