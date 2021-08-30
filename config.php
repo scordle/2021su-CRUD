6b0e795a6b3d2f75a4e175681dd821eb9ec39c78
Scott Cordle <scordle@sullivan.edu>
2021-08-30 12:23:48 -0400

    attempting to connect

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+  * Configuration for database connection
+  *
+  */
+
+$host       = "localhost";
+$username   = "group1_su21";
+$password   = "summer2021";
+$dbname     = "group1_su21_scratchpad"; // will use later
+$dsn        = "mysql:host=$host;dbname=$dbname"; // will use later
+$options    = array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+              );
